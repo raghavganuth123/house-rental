e2229c27d51dd9afc1f5f84a726c0e8c059757ae
raghavganuth123 <92864585+raghavganuth123@users.noreply.github.com>
2024-07-02 10:43:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+document.querySelector('nav').addEventListener('click', function() {
+    this.querySelector('ul').classList.toggle('show');
+});
